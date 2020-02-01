d445ac7d4b2b5a0942dd3f70058922b6435cc238
jack2015 <jack2015@users.noreply.github.com>
2020-02-01 16:45:32 +0800

    Revert "Update reader-streamguard.c"
    This reverts commit 7199f3300d022f9ea6cd2464993a1e5c817ce58e.

--- reader-streamguard.c
+++ reader-streamguard.c
@@ -554,8 +554,6 @@ static int32_t streamguard_card_info(struct s_reader *reader)
 		return ERROR;
 	}
 
-	if(data_len == 0){return ERROR;}
-
 	reader->nprov = 0;
 	int count = ((nextReadSize - 3) / 46) < 4 ? (nextReadSize - 3) / 46 : 4;
 	int i;
@@ -582,30 +580,33 @@ static int32_t streamguard_card_info(struct s_reader *reader)
 	}
 	int bankid=0;
 	for(i = 0; i < reader->nprov; i++){
-		get_subscription_cmd[5] = bankid;
+		int j=0;
 		get_subscription_cmd[10] = reader->prid[i][2];
 		get_subscription_cmd[11] = reader->prid[i][3];
-		write_cmd(get_subscription_cmd, get_subscription_cmd + 5);
-		if((cta_res[cta_lr - 2] & 0xF0) != 0x60){
+                for(;;){
+			get_subscription_cmd[5] = bankid;
+			write_cmd(get_subscription_cmd, get_subscription_cmd + 5);
+			if((cta_res[cta_lr - 2] & 0xF0) != 0x60) {
 				rdr_log(reader, "error:  get subscription failed.");
 				break;
-		}
+			}
 
-		data_len = streamguard_read_data(reader,cta_res[cta_lr - 1], data, &status);
-		if(data_len < 0){
-			rdr_log(reader, "error: read data failed for get subscription.");
-			break;
-		}
+			data_len = streamguard_read_data(reader,cta_res[cta_lr - 1], data, &status);
+			if(data_len < 0){
+				rdr_log(reader, "error: read data failed for get subscription.");
+				break;
+			}
 
-		if(data_len == 0){break;}
+			count = data[1];
+			int k;
+			for(k = 0; k < count; j++,k++){
+				//if(data[j * 19 + 2 + 3] == 0 && data[j * 19 + 3 + 3] == 0) continue;
 
-		int j;
-		count = (int)data[1];
-		for(j = 0; j < count; j++){
 				time_t start_t,end_t,subscription_t;
 				subscription_t = b2i(4, data + 3 + j * 19 + 4);
 				start_t = b2i(4, data + j * 19 + 9 + 3);
-				if((uint32_t)start_t == 0xFFFFFFFFLU) {start_t = subscription_t;}
+				if((uint32_t)start_t == 0xFFFFFFFFLU)
+					start_t = subscription_t;
 				end_t = b2i(4, data + 3 + j * 19 + 13);
 				uint64_t product_id=b2i(2, data + 3 + j * 19 + 2);
 
@@ -620,12 +621,16 @@ static int32_t streamguard_card_info(struct s_reader *reader)
 				strftime(start_day, sizeof(start_day), "%Y-%m-%d %H:%M:%S", &tm_start);
 				strftime(end_day, sizeof(end_day), "%Y-%m-%d %H:%M:%S", &tm_end);
 
-				if(!j){rdr_log(reader, "entitlements for provider: %d (%04X:%06X)", i, reader->caid, b2i(2, &reader->prid[i][2]));}
+				if(!j)
+					rdr_log(reader, "entitlements for provider: %d (%04X:%06X)", i, reader->caid, b2i(2, &reader->prid[i][2]));
 				rdr_log(reader, "    chid: %04"PRIX64" auth:%s  valid:%s - %s", product_id,  subscription_day, start_day, end_day);
 
 				cs_add_entitlement(reader, reader->caid, b2i(2, &reader->prid[i][2]), product_id, 0, start_t, end_t, 0, 1);
+			}
+			if(data[0] == 0)
+				break;
+			bankid = data[0];
 		}
-        bankid = (int)data[0];
 	}
 
 	return OK;
