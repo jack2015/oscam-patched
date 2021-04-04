c2384aac39896645137ee40c51c32c2dcafe27eb
jack2015 <344784320@qq.com>
2021-04-04 21:45:50 +0800

    update to 11691

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -260,7 +260,7 @@ static int dvbapi_ioctl(int fd, uint32_t request, ...)
 			case DMX_STOP:
 			{
 				ret = send(fd, &request, sizeof(request), 0);
-				ret = 1;
+				if(!(ret == 1)){ ret = 1; }
 				break;
 			}
 
@@ -2472,7 +2472,7 @@ void dvbapi_set_pid(int32_t demux_id, int32_t num, uint32_t idx, bool enable, bo
 										{
 											cs_log("ERROR: Could not close demuxer fd (errno=%d %s)", errno, strerror(errno));
 										}
-										currentfd = ca_fd[i] = 0;
+										ca_fd[i] = 0;
 									}
 								}
 							}
@@ -3347,7 +3347,6 @@ void dvbapi_resort_ecmpids(int32_t demux_id)
 			}
 		}
 	}
-	p_order = demux[demux_id].ECMpidcount + 1;
 
 	for(n = 0; n < demux[demux_id].ECMpidcount; n++)
 	{
@@ -7127,7 +7126,6 @@ static void *dvbapi_main_local(void *cli)
 			{
 				if(type[i] == 1)
 				{
-					connfd = -1; // initially no socket to read from
 					uint8_t add_to_poll = 0; // we may need to additionally poll this socket when no PMT data comes in
 
 					if(pfd2[i].fd == listenfd)
@@ -7204,7 +7202,6 @@ static void *dvbapi_main_local(void *cli)
 								}
 							}
 							close(connfd);
-							connfd = -1;
 
 							// last connection closed
 							if(!active_conn && (cfg.dvbapi_listenport || cfg.dvbapi_boxtype == BOXTYPE_PC_NODMX))
